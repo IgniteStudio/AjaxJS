b7e13f895394f3a23b7fe25260dcac8bdfbb0ef9
David Agaybi <davidagaybi@gmail.com>
2018-09-01 20:05:23 -0400

    Created POST form with ajax

--- process.php
+++ process.php
@@ -2,8 +2,15 @@
 
 echo 'Processing...';
 
+// Check for a POST variable
+
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '. $_POST['name'];
+}
+
 // Check for a GET variable
 
 if(isset($_GET['name'])){
     echo 'GET: Your name is '. $_GET['name'];
 }
+
